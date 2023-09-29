06ddd117fa3b5d90bfc664ca573a403b46728457
Ali Maharramli <ali.maharramli.std@bhos.edu.az>
2023-09-29 20:53:45 +0400

    Add Actions and Sonar

--- src/main/java/org/example/Main.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-        PermissionManager perm = new PermissionManager();
-        System.out.println(perm.getmCurrentLevel());
-    }
-}
